c85291cc6a6d3fb8da531939783fd1da584163b5
David Buckley <david@davidbuckley.ca>
2021-05-08 15:32:03 -0400

    fix logging

--- feed_generator/feed.py
+++ feed_generator/feed.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import logging
 import toml
 import os
 from pathlib import Path
@@ -11,6 +12,12 @@ from pydantic import HttpUrl, BaseModel
 from feed_generator.config import Settings, FeedModel
 from feed_generator import generators
 from feed_generator.metadata import fetch_metadata
+
+logging.getLogger("fanficfare").setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+# TODO configurable log level
+logging.basicConfig(level=logging.INFO)
 
 app = typer.Typer()
 
@@ -35,10 +42,11 @@ def generate_feed():
             generator = generators.from_url(url_model.url)
 
             feed_config = FeedModel(url=url_model.url, name=name)
+            logger.info("Generating feed for %s", str(feed_config.url))
             feed = generator.generate_feed(feed_config)
             feed.rss_file(f"{settings.base_dir}/{feed_config.name}.xml")
         except Exception as e:
-            print(url, e)
+            logger.error("%s for %s", e, url_model.url)
 
 @app.command()
 def add(url: str):
